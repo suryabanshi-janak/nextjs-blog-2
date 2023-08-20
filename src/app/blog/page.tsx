30faeb19a98d6126f27dd4fdac914beaa8b2f147
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-08-20 15:20:41 +0545

    Blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,62 @@
-const page = () => {
-  return <div>blog</div>;
+import { formatDate } from '@/lib/utils';
+import { allPosts } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const BlogPage = () => {
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    });
+
+  return (
+    <div className='container max-w-4xl py-10'>
+      <div>
+        <h1 className='inline-block font-bold tracking-tight text-5xl'>Blog</h1>
+        <p className='text-xl text-muted-foreground mt-4'>
+          A blog built using ContentLayer. Posts are written in MDX
+        </p>
+      </div>
+      <hr className='my-8' />
+      {posts.length ? (
+        <div className='grid gap-10 sm:grid-cols-2'>
+          {posts.map((post, index) => (
+            <article
+              key={post._id}
+              className='group relative flex flex-col space-y-2'
+            >
+              {post.image && (
+                <Image
+                  src={post.image}
+                  alt={post.title}
+                  width={804}
+                  height={452}
+                  className='rounded-md border bg-muted transition-colors'
+                  priority={index <= 1}
+                />
+              )}
+              <h2 className='text-2xl font-extrabold'>{post.title}</h2>
+              {post.description && (
+                <p className='text-muted-foreground'>{post.description}</p>
+              )}
+              {post.date && (
+                <p className='text-sm text-muted-foreground'>
+                  {formatDate(post.date)}
+                </p>
+              )}
+              <Link href={post.slug} className='absolute inset-0'>
+                <span className='sr-only'>View Article</span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p>No posts published.</p>
+      )}
+    </div>
+  );
 };
 
-export default page;
+export default BlogPage;
